81f41ff37e3979668d18996f770584922dd17d69
Lixiang Jiang <ljian001@fiu.edu>
2013-09-01 12:58:30 -0700

    Initialized Ciaociao App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ciaociao::Application.config.secret_token = 'fdfd385d0f76959cceb4b3102b3382a7ccbbd2683b9d567091e0c665198c26f4c4beb9ce3b4cfb8db6076a6e722cbe2e3007d38fd9d267741f963b8d62e225ff'
